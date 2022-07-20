a40be0e5ff6f8604ced95acae9ebccea8774cce6
David Gugea <davidgugea@yahoo.com>
2022-07-20 22:06:58 +0200

    Finished Chapter 10

---
+++ SectionII/Chapter10/constexpr_test.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+#define LEN 10
+
+int main() {
+    int x = 42;
+    int arr_1[x];
+
+    arr_1[0] = 42;
+
+    cout << arr_1[0] << endl;
+
+    return 0;
+}
